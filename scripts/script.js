f8ac51988a0d982a99913b6e63a0dda84ba942f6
Isaac Martins <111926496+DevZank@users.noreply.github.com>
2024-05-05 17:44:23 -0300

    Subindo o Projeto

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+const toastBox = document.getElementById("toastBox");
+const sucessMsg =
+  "<i class='ri-checkbox-circle-fill'></i> Sucessfully submitted";
+const errorMsg = "<i class='ri-close-circle-fill'></i> Please fix the error!";
+const invalidMsg =
+  "<i class='ri-error-warning-fill'></i> Invalid input, check again";
+
+function showToast(msg) {
+  let toast = document.createElement("div");
+  toast.classList.add("toast");
+  toast.innerHTML = msg;
+  toastBox.appendChild(toast);
+
+  if (msg.includes("error")) {
+    toast.classList.add("error");
+  }
+
+  if (msg.includes("Invalid")) {
+    toast.classList.add("invalid");
+  }
+
+  setTimeout(() => {
+    toast.remove();
+  }, 6000);
+}
